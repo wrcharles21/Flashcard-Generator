97641dca2dceb3a97ab9cba67b5dd03a22e62796
William Charles <williamcharles@William-Charless-MacBook-Pro.local>
2017-06-29 21:08:50 -0700

    BasicCard.js ClozeCard.js main.js

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+var require = require('./Clozure.js);
+
+function BasicCard(front, back){
+	this.front=front;
+	this.back=back;
+};
+
+module.exports = BasicCard
---
+++ ClozeCard.js
@@ -0,0 +1,12 @@
+
+
+function ClozeCard(text, back){
+	this.text=text.split(cloze);
+	this.cloze=cloze;
+};
+
+function ClozeCardCheck() {
+	
+};
+
+module.exports = ClozeCard;
