979ce67f2ed1c15ace34f3221395a739d7f93386
William Charles <williamcharles@William-Charless-MacBook-Pro.local>
2017-06-29 23:00:19 -0700

    BasicCard.js ClozeCard.js

--- BasicCard.js
+++ BasicCard.js
@@ -1,8 +1,20 @@
-var require = require('./Clozure.js);
 
 function BasicCard(front, back){
 	this.front=front;
 	this.back=back;
 };
 
-module.exports = BasicCard
+
+var president = new BasicCard('Who was the first President of the United States?', 'George Washington');
+	console.log(president.front);
+	console.log(president.back);
+
+var sixteenthPresident = new BasicCard('Who was the 16th President of the United States?', 'Abraham Lincoln');
+	console.log(sixteenthPresident.front);
+	console.log(sixteenthPresident.back);
+
+var currentPresident = new BasicCard('Who is the curent President of the United States?', 'Donald Trump');
+	console.log(currentPresident.front);
+	console.log(currentPresident.back);
+
+module.exports = BasicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,12 +1,27 @@
 
 
-function ClozeCard(text, back){
-	this.text=text.split(cloze);
+function ClozeCard(text, cloze){
 	this.cloze=cloze;
+	this.fullText=text;
+	this.partial=this.fullText.replace(this.cloze,'____');
 };
 
-function ClozeCardCheck() {
-	
-};
+var president = new ClozeCard('George Washington was the  President of the United States?', 'George Washington');
+	console.log(president.fullText);
+	console.log(president.cloze);
+	console.log(president.partial);
+
+var sixteenthPresident = new ClozeCard('Abraham Lincoln was the 16th President of the United States?', 'Abraham Lincoln');
+	console.log(sixteenthPresident.fullText);
+	console.log(sixteenthPresident.cloze);
+	console.log(sixteenthPresident.partial);
+
+var currentPresident = new ClozeCard('Donald Trump is the curent President of the United States?', 'Donald Trump');
+	console.log(currentPresident.fullText);
+	console.log(currentPresident.cloze);
+	console.log(currentPresident.partial)
+
+
+
 
 module.exports = ClozeCard;
